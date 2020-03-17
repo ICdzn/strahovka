2fa3f206ec15897f02b995d4882b24c2f8b426b4
duskmaiden18 <zikzak200015@gmail.com>
2020-03-17 16:29:25 +0200

    first commit

---
+++ strahovka/classes.py
@@ -0,0 +1,114 @@
+from bs4 import BeautifulSoup
+import requests
+
+class Updates:
+
+    def __init__(self,last_id):
+        self.last_id=last_id
+
+    def parser(self):
+
+        companies_info=[]
+        licenses = []
+        id = self.last_id + 1
+
+        r = requests.post(
+            'https://kis.nfp.gov.ua/?__VIEWSTATE=%2FwEPDwUKMjA0NDA5OTAxN2RkYp6DC6WJ1c7OZ5ZQtR%2FzO%2BgjVTw%3D&p_EDRPOU=&p_REGNO='
+            '&p_FULLNAME=&p_IM_ST=%25null%25&p_IRL_FT=3&p_NFS=0&p_SVIDOTSTVO_SERIES=&p_SVIDOTSTVO_NO=&p_ACTDATE_FROM=&p_ACTDATE_TO=&p_ILD_NUMBER='
+            '&pagenum=1&btn_all=1&__VIEWSTATEGENERATOR=EC5CD28C')
+
+        html = r.text
+        soup = BeautifulSoup(html,'lxml')
+
+        table = soup.find('table', class_='grid zebra')
+        tr_list = table.find_all('tr')
+
+        for tr in tr_list:
+            if tr.find_all('td'):
+                td_list=tr.find_all('td')
+                dict_company = {'id':id}
+                for td in td_list:
+                    if td.find('a') and 'Детально' in td.text:
+                         abbreviation,position=self.get_details(td.find('a').get('href'))
+                         dict_company['abbreviation'] = abbreviation.strip()
+                         director = director.replace(abbreviation.lower().strip(), ' ')
+                         director_title=director.title()
+                         dict_company['K_NAME'] = director_title
+                         director=director.strip()
+                         position = position.replace(',', ' ')
+                         position = position.replace('-', ' ')
+                         position = position.lower().replace(director, ' ')
+                         dict_company['position'] = position.title()
+                    elif td.get('headers')[0]=="K_NAME":
+                        td.text.lower()
+                        posada=['генеральний директор','виконуючий обов`язки голови правління','в.о. голови правління','директор',
+                                'голова правління','гол. правління','президент']
+                        director=td.text.lower()
+                        for i in posada:
+                            if i in director:
+                                director=director.replace(i,' ')
+                        director = director.replace('-', ' ')
+                        director = director.replace(',', ' ')
+                        dict_company['K_NAME'] = director
+                    elif td.find('a') and 'Ліцензії' in td.text:
+                        licenses=self.get_license(td.find('a').get('href'),id,licenses)
+                    elif td.get('headers')[0]=="FILIALS":
+                        continue
+                    else:
+                        dict_company[td.get('headers')[0]] = td.text
+                companies_info.append(dict_company)
+                id += 1
+        return (companies_info,licenses)
+
+    def get_details(self,url):
+        new_url = 'https://kis.nfp.gov.ua' + url
+        html = requests.get(new_url).text
+        soup = BeautifulSoup(html, 'lxml')
+
+        abbreviation=soup.find(text='Скорочене найменування заявника (з установчих документів, у разі наявності)').parent.findNext('td').contents[0]
+        position=soup.find(text="Прізвище, і'мя та по батькові і найменування посади керівника").parent.findNext('td').contents[0]
+        return abbreviation,position
+
+    def get_license(self,url,id,table_info):
+        names = []
+        just_names = []
+
+        new_url = 'https://kis.nfp.gov.ua' + url
+        html = requests.get(new_url).text
+        soup = BeautifulSoup(html, 'lxml')
+
+        table = soup.find('table', class_='grid zebra')
+        try:
+            tr_list = table.find_all('tr')
+            index = 0
+            for tr in tr_list:
+                if tr.find_all('th'):
+                    th_list=tr.find_all('th')
+                    for th in th_list:
+                        names.append({th.get('id'):th.text})
+                        just_names.append(th.get('id'))
+                else:
+                    td_list=tr.find_all('td')
+                    dict_company = {}
+                    for td in td_list:
+                        if td.get('a'):
+                            dict_company[just_names[index]] = td.find('a').get('href')
+                        else:
+                            dict_company[just_names[index]] = td.text
+                        index += 1
+                    dict_company['company_id']=id
+                    table_info.append(dict_company)
+                    index = 0
+        except: pass
+        return table_info
+
+    def compare(self,rows,companies):
+         changes=''
+         for row in rows:
+             i=0
+             for key in companies[0]:
+                 if row[key] != companies[i][key]:
+                     changes+=key+','
+             companies[i]['changes']=changes
+             i+=1
+
